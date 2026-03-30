371ee155bf0e41791da8195ba2efbf20169941ef
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2026-03-30 00:03:52 -0400

    Fix models/__init__.py: remove stale imports that triggered missing module errors

--- gen_model/models/__init__.py
+++ gen_model/models/__init__.py
@@ -1,6 +1,1 @@
-"""Models module for SE(3) diffusion score networks."""
-
-from gen_model.models.score_network import ScoreNetwork
-from gen_model.models.ipa_pytorch import IpaScore, InvariantPointAttention
-
-__all__ = ['ScoreNetwork', 'IpaScore', 'InvariantPointAttention']
+"""Models module for STAR-MD score networks."""
